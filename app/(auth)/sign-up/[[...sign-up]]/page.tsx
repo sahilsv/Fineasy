1222a5e7b9d9f2c7f6a5fed1bbc0e47b09db6ba8
Sahil Singh Virdhi <sahilsv003@gmail.com>
2024-10-25 22:59:36 +0530

    hono.js setup done

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
       <div className="h-full lg:flex flex-col items-center justify-center px-4">
-        <div className="text-center space-y-4 pt-4">
+        <div className="text-center space-y-4 pt-2">
           <h1 className="font-bold text-3xl text-[#2f3037]">Welcome!</h1>
           <p className="text-base text-[#7E8CA0]">
             Create account to access your dashboard
